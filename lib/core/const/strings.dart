e9ac42eb2b7e3af2e0974da0917858fb3a2f0cf7
German <84451221+VadimFDW@users.noreply.github.com>
2024-02-08 22:47:27 +0200

    Have added initial wraper screen, botom navBar and some constants

--- lib/core/const/strings.dart
+++ lib/core/const/strings.dart
@@ -1,4 +1,7 @@
 class AppStrings {
   static const String hi = 'Привіт!';
-
+  static const String contacts = 'Contacts';
+  static const String home = 'Home';
+  static const String map = 'Map';
+  static const String profile = 'Profile';
 }
---
+++ lib/core/router/routes.dart
@@ -0,0 +1,6 @@
+class Routes {
+  static const contactsIndex = 0;
+  static const homeIndex = 1;
+  static const mapIndex = 2;
+  static const profileIndex = 3;
+}
