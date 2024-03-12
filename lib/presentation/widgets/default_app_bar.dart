40e95457ad66a686f0f27543c0c2306ea019857c
German <84451221+VadimFDW@users.noreply.github.com>
2024-03-13 01:18:00 +0200

    fixed UI

--- lib/presentation/widgets/default_app_bar.dart
+++ lib/presentation/widgets/default_app_bar.dart
@@ -1,13 +1,12 @@
-
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:sicherr/core/const/colors.dart';
 import 'package:sicherr/core/theme/theme.dart';
 
 class DefaultAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const DefaultAppBar({super.key, required this.title, this.icon});
+  const DefaultAppBar({super.key, this.title, this.icon});
 
-  final String title;
+  final String? title;
   final Widget? icon;
 
   @override
@@ -18,36 +17,30 @@ class DefaultAppBar extends StatelessWidget implements PreferredSizeWidget {
     return AppBar(
       surfaceTintColor: AppColors.white,
       backgroundColor: AppColors.lightGrey,
-      automaticallyImplyLeading: false,
-      title: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          InkWell(
-            onTap: () {
-              Navigator.pop(context);
-            },
-            child: const SizedBox(
-              width: 25,
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 8.0),
-                child: FaIcon(
-                  FontAwesomeIcons.chevronLeft,
-                  color: AppColors.black,
-                  size: 20,
-                ),
-              ),
+      leading: GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        onTap: () {
+          Navigator.pop(context);
+        },
+        child: const Center(
+          child: Padding(
+            padding: EdgeInsets.only(left: 5),
+            child: FaIcon(
+              FontAwesomeIcons.chevronLeft,
+              color: AppColors.black,
+              size: 23,
             ),
           ),
-          Expanded(
-            child: Text(
-              title,
-              style: AppTheme.themeData.textTheme.displayLarge,
-              textAlign: TextAlign.center,
-            ),
-          ),
-          if (icon != null) icon!
-        ],
+        ),
       ),
+      automaticallyImplyLeading: false,
+      centerTitle: true,
+      title: Text(
+        title ?? '',
+        style: AppTheme.themeData.textTheme.displayLarge,
+        textAlign: TextAlign.center,
+      ),
+      actions: [if (icon != null) icon!],
     );
   }
 }
