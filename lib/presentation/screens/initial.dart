1fe043dd27cc1d4106fe61e5d1dbf1bfc89f6b62
Dmitriy Plashkin <66801350+Dmitriy1205@users.noreply.github.com>
2024-04-03 18:04:16 +0300

    fix

--- lib/presentation/screens/initial.dart
+++ lib/presentation/screens/initial.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:sicherr/core/const/colors.dart';
 import 'package:sicherr/core/const/icons.dart';
+import 'package:sicherr/core/managers/contacts_manager.dart';
 import 'package:sicherr/core/managers/quick_binding_handler.dart';
 import 'package:sicherr/core/theme/theme.dart';
 import 'package:sicherr/presentation/bloc/profile/profile_bloc.dart';
@@ -16,6 +17,7 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:sicherr/presentation/widgets/sos_confirmation_popup.dart';
 
 import '../../core/service_locator/service_locator.dart';
+import '../bloc/contacts/contacts_bloc.dart';
 import '../bloc/emergency_contact/emergency_contact_bloc.dart';
 import '../bloc/onboarding/onboarding_bloc.dart';
 import '../widgets/app_toast.dart';
@@ -43,6 +45,7 @@ class _InitialScreenState extends State<InitialScreen> {
 
     context.read<OnboardingBloc>().add(const OnboardingEvent.get());
     // context.read<ContactsBloc>().add(ContactsEvent.initial());
+    // sl<ContactsInterface>().getContacts();
     context
         .read<EmergencyContactBloc>()
         .add(const EmergencyContactEvent.getAllEmContacts());
