2101dadc251c469ff2eabbe36038af810ce57c3e
PandasCodi <61013205+Majestr32@users.noreply.github.com>
2024-04-06 19:28:40 +0300

    merge resolved

--- lib/core/managers/contacts_manager.dart
+++ lib/core/managers/contacts_manager.dart
@@ -20,9 +20,13 @@ class ContactsManager implements ContactsInterface {
       final localContacts = await ContactsService.getContacts(photoHighResolution: false);
 
       for (var element in localContacts) {
-        final contact = ContactEntity.fromLocalContact(element);
-        if (contact.phoneNumber.isNotEmpty) {
-          contacts.add(contact);
+        try{
+          final contact = ContactEntity.fromLocalContact(element);
+          if (contact.phoneNumber.isNotEmpty) {
+            contacts.add(contact);
+          }
+        }catch(_){
+
         }
       }
     } else if (permission.isPermanentlyDenied) {
--- lib/domain/repositories/contacts/contacts_repository_impl.dart
+++ lib/domain/repositories/contacts/contacts_repository_impl.dart
@@ -126,9 +126,13 @@ class ContactsRepositoryImpl implements ContactsRepository {
       final localContacts = await ContactsService.getContacts();
 
       for (var element in localContacts) {
-        final contact = ContactEntity.fromLocalContact(element);
-        if (contact.phoneNumber.isNotEmpty) {
-          contacts.add(contact);
+        try{
+          final contact = ContactEntity.fromLocalContact(element);
+          if (contact.phoneNumber.isNotEmpty) {
+            contacts.add(contact);
+          }
+        }catch(_){
+
         }
       }
     } else if (permission.isPermanentlyDenied) {
